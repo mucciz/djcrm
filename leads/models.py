3bc192e80f25c10e8b3a0202607d1f56c40ae0a4
Kenneth Muchiri <kenmucciz8@gmail.com>
2021-04-01 12:10:05 +0300

    Added reverse urls functionality to the model

--- leads/models.py
+++ leads/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
 class User(AbstractUser):
@@ -12,6 +13,8 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     agent = models.ForeignKey('Agent', on_delete=models.CASCADE)
 
+    def get_absolute_url(self):
+        return reverse('leads:lead_detail', args=[self.pk])
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
