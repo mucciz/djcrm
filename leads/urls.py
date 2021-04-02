8a320657c8bc6e87f77940d5e635129d07dc0fc3
Kenneth Muchiri <kenmucciz8@gmail.com>
2021-04-02 08:58:34 +0300

    Added urls for CRUD ops

--- leads/urls.py
+++ leads/urls.py
@@ -5,5 +5,8 @@ app_name = 'leads'
 
 urlpatterns = [
     path('', views.lead_list, name='lead_list'),
-    path('<pk>/', views.lead_detail, name='lead_detail')
+    path('create/', views.lead_create, name='lead_create'),
+    path('<int:pk>/', views.lead_detail, name='lead_detail'),
+    path('<int:pk>/update/', views.lead_update, name='lead_update'),
+    path('<int:pk>/delete/', views.lead_delete, name='lead_delete'),
 ]
