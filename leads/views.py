e2ba0f11480801fbf887900f0b18a1b347391ce9
Kenneth Muchiri <kenmucciz8@gmail.com>
2021-04-02 08:59:20 +0300

    Added views for the CRUD ops

--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
-from .models import Lead
+from .models import Lead, Agent
+from .forms import LeadModelForm, LeadForm
 
 
 def lead_list(request):
@@ -20,3 +21,41 @@ def lead_detail(request, pk):
     }
     # print(reverse('leads:lead_detail', args=[pk]))
     return render(request, 'leads/lead_detail.html', context)
+
+
+def lead_create(request):
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/leads')
+
+    else:
+        form = LeadModelForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'leads/lead_create.html', context)
+
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect('/leads')
+    else:
+        form = LeadModelForm(instance=lead)
+    context = {
+        'form': form,
+        'lead': lead
+    }
+
+    return render(request, 'leads/lead_update.html', context)
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect('/leads')
