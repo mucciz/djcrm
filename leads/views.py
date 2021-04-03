69aae28a412e0fecb8a88e98f3cb29907b80027d
Kenneth Muchiri <kenmucciz8@gmail.com>
2021-04-03 21:28:21 +0300

    Added email support

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.views.generic import (
     TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -29,6 +30,16 @@ class LeadCreateView(CreateView):
 
     def get_success_url(self):
         return reverse('leads:lead_list')
+
+    def form_valid(self, form):
+        send_mail(
+            subject='A lead has been created',
+            message='Go to the site to check it out',
+            from_email='test@testUser.com',
+            recipient_list=['test@testAdmin.com'],
+            fail_silently=False
+        )
+        return super().form_valid(form)
 
 
 class LeadUpdtaeView(UpdateView):
