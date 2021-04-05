3e61b35a850f3bb14159b4ac9e448ef6a5df1b6e
Kenneth Muchiri <kenmucciz8@gmail.com>
2021-04-05 14:45:45 +0300

    Added Log I/O urls and redirect urls

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -126,3 +126,6 @@ STATIC_ROOT = 'static_root'
 AUTH_USER_MODEL = 'leads.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+LOGIN_REDIRECT_URL = '/leads'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
