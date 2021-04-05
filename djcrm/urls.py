bcfed25bb82b7e697da55e741f68244ea4ac0b58
Kenneth Muchiri <kenmucciz8@gmail.com>
2021-04-05 14:46:59 +0300

    Wired up Auth urls

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,15 +1,22 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.views import (
+    LoginView, LogoutView,
+    )
 from django.urls import path, include
-from leads.views import LandingPageView
+from leads.views import LandingPageView, SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='landing_page'),
     path('leads/', include('leads.urls', namespace='leads')),
+    path('signup/', SignUpView.as_view(), name='sign_up'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
